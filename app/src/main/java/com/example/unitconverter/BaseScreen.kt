8e4f6d27f5d99b13627045693a3157fb8b23ef3d
ron18923 <ronron18923@gmail.com>
2023-04-26 15:26:15 +0300

    basic structure and functioning drop-down menu working

---
+++ app/src/main/java/com/example/unitconverter/BaseScreen.kt
@@ -0,0 +1,23 @@
+package com.example.unitconverter
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun BaseScreen(
+    modifier: Modifier = Modifier,
+    converterViewModel: ConverterViewModel = viewModel()
+) {
+    val list = converterViewModel.getConversions()
+    Column(modifier = modifier.padding(30.dp)) {
+        TopScreen(list)
+        Spacer(modifier = modifier.height(20.dp))
+        HistoryScreen()
+    }
+}
